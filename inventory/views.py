d4a20bd57df589e2e3ae555a78c01d033edc23b2
Andreas Zweili <andreas@2li.ch>
2017-12-25 00:37:52 +0100

    add an index view

--- inventory/views.py
+++ inventory/views.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 from django.shortcuts import get_object_or_404, render
+
+
+def index(request):
+    device_list = Device.objects.all()
+    computer_list = Computer.objects.all()
+
+    return render(request,
+                  'inventory/index.html',
+                  {'device_list': category_list,
+                   'computer_list': computer_list})
