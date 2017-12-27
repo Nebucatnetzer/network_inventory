8ea0a1cd6151bbf7e165722dd72ae9aa90b92edc
Andreas Zweili <andreas@2li.ch>
2017-12-27 20:17:12 +0100

    add settings for mariadb

--- network_inventory/network_inventory/settings.py
+++ network_inventory/network_inventory/settings.py
@@ -76,11 +76,17 @@ WSGI_APPLICATION = 'network_inventory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'inventorydb',
+        'USER': 'inventory',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
-}
-
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
