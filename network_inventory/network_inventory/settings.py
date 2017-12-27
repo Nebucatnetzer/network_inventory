2fbcf1e5f77ab479eb8da7a254392423b3ac01d3
Andreas Zweili <andreas@2li.ch>
2017-12-27 23:51:15 +0100

    remove the mysql settings

--- network_inventory/network_inventory/settings.py
+++ network_inventory/network_inventory/settings.py
@@ -71,22 +71,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'network_inventory.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'inventorydb',
-        'USER': 'inventory',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
- }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
