e6dc844aa6ec965f8fd78930e9370b3497742dc0
Andreas Zweili <andreas@2li.ch>
2017-12-27 20:02:51 +0100

    update the setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
-rm inventory/migrations/*.py
-source bin/activate
+rm network_inventory/inventory/migrations/*.py
+python3 -m venv network_inventory
+source network_inventory/bin/activate
 pip3 install -r requirements.txt
-python3 manage.py makemigrations inventory
-python3 manage.py migrate
+python3 network_inventory/manage.py makemigrations inventory
+python3 network_inventory/manage.py migrate
 echo "from django.contrib.auth.models import User; \
       User.objects.filter(email='admin@example.com').delete(); \
       User.objects.create_superuser('admin', 'admin@example.com', 'password')" |
-      python3 manage.py shell
-python3 manage.py loaddata inventory
+      python3 network_inventory/manage.py shell
+python3 network_inventory/manage.py loaddata inventory
