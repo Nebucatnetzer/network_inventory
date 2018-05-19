cf79504d5db995885d00b173d8dbd71485623c9b
Andreas Zweili <andreas@2li.ch>
2018-05-19 09:27:23 +0200

    fix paths in the setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
-rm network_inventory/inventory/migrations/*.py
-python3 -m venv network_inventory
-source network_inventory/bin/activate
+rm /vagrant/network_inventory/inventory/migrations/*.py
+python3 -m venv /vagrant/network_inventory
+source /vagrant/network_inventory/bin/activate
 pip3 install -r requirements.txt
-python3 network_inventory/manage.py makemigrations inventory
-python3 network_inventory/manage.py migrate
+python3 /vagrant/network_inventory/manage.py makemigrations inventory
+python3 /vagrant/network_inventory/manage.py migrate
 echo "from django.contrib.auth.models import User; \
       User.objects.filter(email='admin@example.com').delete(); \
       User.objects.create_superuser('admin', 'admin@example.com', 'password')" |
-      python3 network_inventory/manage.py shell
-python3 network_inventory/manage.py loaddata inventory
+      python3 /vagrant/network_inventory/manage.py shell
+python3 /vagrant/network_inventory/manage.py loaddata inventory
