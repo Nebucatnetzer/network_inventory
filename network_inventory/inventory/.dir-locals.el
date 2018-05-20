50614938a7069aaf1a5f9f6f2721a6346772c8b7
Andreas Zweili <andreas@2li.ch>
2018-05-20 14:04:26 +0200

    add a directory configuration

---
+++ network_inventory/inventory/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((python-mode
+  (pyvenv-activate . "~/git_repos/projects/network_inventory/network_inventory")))
