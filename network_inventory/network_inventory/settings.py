9647da27f8c00ba77f43957db65f2b8f97183a14
Andreas Zweili <andreas@2li.ch>
2019-06-10 22:24:48 +0200

    remove the STATIC_ROOT option
    django throws a lot of errors with it.

--- network_inventory/network_inventory/settings.py
+++ network_inventory/network_inventory/settings.py
@@ -125,8 +125,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = 'static/'
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
