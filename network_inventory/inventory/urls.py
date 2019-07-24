e162ec4177d64fe7c3bd44b7786f50ff5fa9dd28
Andreas Zweili <andreas@2li.ch>
2019-07-24 22:38:19 +0200

    PEP8, line length

--- network_inventory/inventory/urls.py
+++ network_inventory/inventory/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('customer/<int:customer_id>/', views.ComputerList.as_view(),
          name='computers'),
     path('device/<int:device_id>/', views.device_details, name='device'),
-    path('computer/<int:computer_id>/', views.computer_details, name='computer'),
+    path('computer/<int:computer_id>/', views.computer_details,
+         name='computer'),
     path('devices/', views.DeviceList.as_view(), name='devices'),
  ]
