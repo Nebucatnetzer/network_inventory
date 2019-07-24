f9dd215e78ec9a1177a92115b155b1af045f6e68
Andreas Zweili <andreas@2li.ch>
2019-07-24 23:13:40 +0200

    load fixtures only once with sqlite DB

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cd network_inventory
-python manage.py makemigrations
-python manage.py migrate
-python manage.py loaddata inventory
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')"
+if [ -f ./network_inventory/db.sqlite3 ]; then
+    python manage.py makemigrations
+    python manage.py migrate
+else
+    python manage.py makemigrations
+    python manage.py migrate
+    python manage.py loaddata inventory
+    python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')"
+fi
 python manage.py runserver 0.0.0.0:8000
