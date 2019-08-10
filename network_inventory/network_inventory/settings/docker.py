b9efd4ed60716068015e94f3031c14dde1c4e37f
Andreas Zweili <andreas@2li.ch>
2019-08-10 16:50:35 +0200

    move some settings to environment variables

--- network_inventory/network_inventory/settings/docker.py
+++ network_inventory/network_inventory/settings/docker.py
@@ -5,11 +5,10 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'development_key'
+DEBUG = os.environ.get('DEBUG')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DATABASES = {
     'default': {
