c1631fe76e21ee52b43206d61d9ab2ab3934adc7
Andreas Zweili <andreas@2li.ch>
2019-08-10 13:55:15 +0200

    use a in memory database for local testing

--- network_inventory/network_inventory/settings/local.py
+++ network_inventory/network_inventory/settings/local.py
@@ -12,8 +12,8 @@ SECRET_KEY = 'development_key'
 DEBUG = True
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': ':memory:',
+  }
 }
