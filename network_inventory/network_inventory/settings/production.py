521aaef94660a329faa135114e71a342f9d0ab87
Andreas Zweili <andreas@2li.ch>
2019-08-11 18:37:37 +0200

    run django with gunicorn

--- network_inventory/network_inventory/settings/production.py
+++ network_inventory/network_inventory/settings/production.py
@@ -5,7 +5,7 @@ ALLOWED_HOSTS = [
 ]
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kzx(i9^@*g^cgp(_3052%*1d%zyu^2z_@pgso5!_q@jb-j%4m='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
