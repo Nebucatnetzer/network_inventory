9d859f4ffc836d4f3c223686c7cb5bec0f9078e9
Andreas Zweili <andreas@2li.ch>
2019-08-25 22:35:03 +0200

    add login functionality

--- network_inventory/inventory/urls.py
+++ network_inventory/inventory/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', views.customers_table_view, name='customers'),
     path('customer/<int:pk>/', views.CustomerDetailView.as_view(),
          name='customer'),
