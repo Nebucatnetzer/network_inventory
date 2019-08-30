e314c7bf47390d66af0fd0bb3791aa045489e5cb
Andreas Zweili <andreas@2li.ch>
2019-08-30 23:14:47 +0200

    fix the tests so that they are working with postgres

--- network_inventory/inventory/tests/test_views.py
+++ network_inventory/inventory/tests/test_views.py
@@ -49,15 +49,16 @@ def test_customer_detail_view_not_found(create_admin_user):
     create_admin_user()
     client = Client()
     client.login(username="novartis-admin", password="password")
-    response = client.get('/customer/2/')
+    response = client.get('/customer/230/')
     assert response.status_code == 404
 
 
 def test_customer_detail_view(create_admin_user):
     fixture = create_admin_user()
+    customer = fixture['customer']
     client = Client()
     client.login(username="novartis-admin", password="password")
-    response = client.get('/customer/1/')
+    response = client.get('/customer/' + str(customer.id) + '/')
     assert (response.status_code == 200
             and in_content(response, fixture['customer'].name))
 
@@ -69,7 +70,7 @@ def test_customer_detail_view_no_permissions():
     client = Client()
     customer = mixer.blend('inventory.Customer')
     client.login(username="novartis-admin", password="password")
-    response = client.get('/customer/1/')
+    response = client.get('/customer/' + str(customer.id) + '/')
     assert response.status_code == 302 and 'login' in response.url
 
 
@@ -79,19 +80,21 @@ def test_customer_computer_table_not_logged_in():
 
 
 def test_customer_computer_table(create_admin_user):
-    create_admin_user()
+    fixture = create_admin_user()
+    customer = fixture['customer']
     client = Client()
     client.login(username="novartis-admin", password="password")
     computer = mixer.blend('inventory.Computer', customer=mixer.SELECT)
-    response = client.get('/customer/1/computers/')
+    response = client.get('/customer/' + str(customer.id) + '/computers/')
     assert response.status_code == 200 and in_content(response, computer.name)
 
 
 def test_customer_computer_table_no_computer(create_admin_user):
-    create_admin_user()
+    fixture = create_admin_user()
+    customer = fixture['customer']
     client = Client()
     client.login(username="novartis-admin", password="password")
-    response = client.get('/customer/1/computers/')
+    response = client.get('/customer/' + str(customer.id) + '/computers/')
     assert response.status_code == 200 and not_in_content(response, "Novartis PC")
 
 
@@ -110,7 +113,7 @@ def test_computer_detail_view(create_admin_user):
     computer = mixer.blend('inventory.Computer', customer=mixer.SELECT)
     client = Client()
     client.login(username="novartis-admin", password="password")
-    response = client.get('/computer/1/')
+    response = client.get('/computer/' + str(computer.id) + '/')
     assert response.status_code == 200 and in_content(response, computer.name)
 
 
@@ -130,7 +133,7 @@ def test_computer_detail_view_ram_relation(create_admin_user):
     mixer.blend('inventory.ComputerRamRelation', computer=computer, ram=ram)
     client = Client()
     client.login(username="novartis-admin", password="password")
-    response = client.get('/computer/1/')
+    response = client.get('/computer/' + str(computer.id) + '/')
     assert response.status_code == 200 and in_content(response, "RAM Modules:")
 
 
@@ -143,7 +146,7 @@ def test_computer_detail_view_raid_relation(create_admin_user):
     mixer.blend('inventory.RaidInComputer', computer=computer, raid=disks)
     client = Client()
     client.login(username="novartis-admin", password="password")
-    response = client.get('/computer/1/')
+    response = client.get('/computer/' + str(computer.id) + '/')
     assert response.status_code == 200 and in_content(response, "RAID")
 
 
@@ -154,7 +157,7 @@ def test_computer_detail_view_cpu_relation(create_admin_user):
     mixer.blend('inventory.ComputerCpuRelation', cpu=cpu, computer=computer)
     client = Client()
     client.login(username="novartis-admin", password="password")
-    response = client.get('/computer/1/')
+    response = client.get('/computer/' + str(computer.id) + '/')
     assert response.status_code == 200 and in_content(response, cpu.name)
 
 
@@ -168,7 +171,7 @@ def test_device_detail_view(create_admin_user):
     device = mixer.blend('inventory.Device', customer=mixer.SELECT)
     client = Client()
     client.login(username="novartis-admin", password="password")
-    response = client.get('/device/1/')
+    response = client.get('/device/' + str(device.id) + '/')
     assert response.status_code == 200 and in_content(response, device.name)
 
 
@@ -190,7 +193,7 @@ def test_net_detail_view(create_admin_user):
     net = mixer.blend('inventory.Net')
     client = Client()
     client.login(username="novartis-admin", password="password")
-    response = client.get('/net/1/')
+    response = client.get('/net/' + str(net.id) + '/')
     assert response.status_code == 200 and in_content(response, net.name)
 
 
@@ -214,7 +217,7 @@ def test_backup_detail_view(create_admin_user):
     backup = mixer.blend('inventory.Backup', computer=computer)
     client = Client()
     client.login(username="novartis-admin", password="password")
-    response = client.get('/backup/1/')
+    response = client.get('/backup/' + str(backup.id) + '/')
     assert response.status_code == 200 and in_content(response, backup.name)
 
 
