e012e1d576327e32b179b1383e1218def513368a
Andreas Zweili <andreas@2li.ch>
2019-08-30 21:04:00 +0200

    add a computer to test

--- network_inventory/inventory/tests/test_views.py
+++ network_inventory/inventory/tests/test_views.py
@@ -251,8 +251,9 @@ def test_computer_list_view_no_computers(create_admin_user):
 
 
 def test_computer_list_view(create_admin_user):
-    create_admin_user()
-    computer = mixer.blend('inventory.Computer')
+    fixture = create_admin_user()
+    computer = mixer.blend('inventory.Computer',
+                           customer=fixture['customer'])
     client = Client()
     client.login(username="novartis-admin", password="password")
     response = client.get('/computers/all/')
