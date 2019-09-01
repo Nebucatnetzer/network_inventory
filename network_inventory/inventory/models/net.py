69d9a24ff7a61ede21e8bc4837e1ced2f02898d0
Andreas Zweili <andreas@2li.ch>
2019-09-01 12:10:51 +0200

    add a description field

--- network_inventory/inventory/models/net.py
+++ network_inventory/inventory/models/net.py
@@ -15,6 +15,7 @@ class Net(models.Model):
     ip_range = models.CharField(max_length=50, verbose_name='IP Range')
     dhcp_range = models.CharField(max_length=50, verbose_name='DHCP Range')
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
