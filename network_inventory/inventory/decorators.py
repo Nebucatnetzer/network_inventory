be6c5dab7f06d080ca416d62ba751614b49cee1f
Andreas Zweili <andreas@2li.ch>
2019-11-30 14:14:01 +0100

    add a decorator to check for the customer_view_permission

--- network_inventory/inventory/decorators.py
+++ network_inventory/inventory/decorators.py
@@ -43,3 +43,16 @@ def connect_device_view_permission(old_function):
                 "You're not allowed to access this device."
                 )
     return new_function
+
+
+def customer_view_permission(old_function):
+    def new_function(request, pk, *args, **kwargs):
+        customer = Customer.objects.get(pk=pk)
+        user = request.user
+        if user.has_perm('inventory.view_customer', customer):
+            return old_function(request, pk)
+        else:
+            return HttpResponseForbidden(
+                "You're not allowed to access this page."
+                )
+    return new_function
