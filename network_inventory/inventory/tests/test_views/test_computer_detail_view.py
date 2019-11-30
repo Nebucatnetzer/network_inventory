6e17f41d83d184fe420353f438c643e901bd0527
Andreas Zweili <andreas@2li.ch>
2019-11-30 16:36:13 +0100

    rewrite the test to actually test the computer view
    For some reason this test was testing the customer detail view but this is now
    fixed.

--- network_inventory/inventory/tests/test_views/test_computer_detail_view.py
+++ network_inventory/inventory/tests/test_views/test_computer_detail_view.py
@@ -9,35 +9,36 @@ import helper
 pytestmark = pytest.mark.django_db
 
 
-def test_customer_detail_view_not_logged_in():
-    response = Client().get('/customer/1/')
+def test_computer_detail_view_not_logged_in():
+    response = Client().get('/computer/1/')
     assert response.status_code == 302 and 'login' in response.url
 
 
-def test_customer_detail_view_not_found(create_admin_user):
+def test_computer_detail_view_not_found(create_admin_user):
     create_admin_user()
     client = Client()
     client.login(username="novartis-admin", password="password")
-    response = client.get('/customer/230/')
+    response = client.get('/computer/230/')
     assert response.status_code == 404
 
 
-def test_customer_detail_view(create_admin_user):
-    fixture = create_admin_user()
-    customer = fixture['customer']
+def test_computer_detail_view(create_admin_user):
+    create_admin_user()
+    computer = mixer.blend('inventory.Computer', customer=mixer.SELECT)
     client = Client()
     client.login(username="novartis-admin", password="password")
-    response = client.get('/customer/' + str(customer.id) + '/')
+    response = client.get('/computer/' + str(computer.id) + '/')
     assert (response.status_code == 200
-            and helper.in_content(response, customer.name))
+            and helper.in_content(response, computer.name))
 
 
-def test_customer_detail_view_no_permissions():
+def test_computer_detail_view_no_permissions():
     User = get_user_model()
     User.objects.create_user("novartis-admin", "admin@novartis.com",
                              "password", is_staff=True)
     client = Client()
-    customer = mixer.blend('inventory.Customer')
+    mixer.blend('inventory.Customer')
+    computer = mixer.blend('inventory.Computer', customer=mixer.SELECT)
     client.login(username="novartis-admin", password="password")
-    response = client.get('/customer/' + str(customer.id) + '/')
-    assert response.status_code == 302 and 'login' in response.url
+    response = client.get('/computer/' + str(computer.id) + '/')
+    assert response.status_code == 403
