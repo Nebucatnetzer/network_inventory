3d52e622749d3110bb86a544cded8217c6c5d683
Andreas Zweili <andreas@2li.ch>
2019-12-11 18:06:31 +0100

    add tests vor net_table_view and net_detail_view

---
+++ network_inventory/inventory/tests/test_views/test_customer_net_table_view.py
@@ -0,0 +1,44 @@
+import pytest
+from mixer.backend.django import mixer
+
+from django.test import Client
+
+import helper
+
+from inventory.models import Customer
+
+pytestmark = pytest.mark.django_db
+
+
+def test_customer_net_table_not_logged_in():
+    response = Client().get('/customer/1/nets/')
+    assert response.status_code == 302 and 'login' in response.url
+
+
+def test_customer_net_table(create_admin_user):
+    create_admin_user()
+    client = Client()
+    client.login(username="novartis-admin", password="password")
+    net = mixer.blend('inventory.Net', customer=mixer.SELECT)
+    response = client.get('/customer/' + str(net.customer.id) + '/nets/')
+    assert (response.status_code == 200
+            and helper.in_content(response, net.name))
+
+
+def test_customer_net_table_no_net(create_admin_user):
+    fixture = create_admin_user()
+    customer = fixture['customer']
+    client = Client()
+    client.login(username="novartis-admin", password="password")
+    response = client.get('/customer/' + str(customer.id) + '/nets/')
+    assert response.status_code == 200
+
+
+def test_customer_net_table_no_permission(create_admin_user):
+    create_admin_user()
+    customer = Customer.objects.create(name='Nestle')
+    client = Client()
+    client.login(username="novartis-admin", password="password")
+    mixer.blend('inventory.Net', customer=customer)
+    response = client.get('/customer/' + str(customer.id) + '/nets/')
+    assert response.status_code == 403
