cc17502806845cf274e65a63adf70043a0630d9a
Andreas Zweili <andreas@2li.ch>
2019-12-28 15:07:59 +0100

    extend the test_device_detail_view

--- network_inventory/inventory/tests/test_views/test_device_detail_view.py
+++ network_inventory/inventory/tests/test_views/test_device_detail_view.py
@@ -15,12 +15,24 @@ def test_device_detail_view_not_logged_in():
 
 def test_device_detail_view(create_admin_user):
     create_admin_user()
-    device = mixer.blend('inventory.Device', customer=mixer.SELECT)
+    mixer.blend('inventory.DeviceCategory')
+    mixer.blend('inventory.Owner')
+    mixer.blend('inventory.Location')
+    device = mixer.blend('inventory.Device', customer=mixer.SELECT,
+                         owner=mixer.SELECT, category=mixer.SELECT,
+                         manufacturer=mixer.SELECT,
+                         location=mixer.SELECT)
     client = Client()
     client.login(username="novartis-admin", password="password")
     response = client.get('/device/' + str(device.id) + '/')
     assert (response.status_code == 200
-            and helper.in_content(response, device.name))
+            and helper.in_content(response, str(device))
+            and helper.in_content(response, str(device.serialnumber))
+            and helper.in_content(response, str(device.category))
+            and helper.in_content(response, str(device.owner))
+            and helper.in_content(response, str(device.customer))
+            and helper.in_content(response, str(device.manufacturer))
+            and helper.in_content(response, str(device.location)))
 
 
 def test_device_detail_view_not_found(create_admin_user):
@@ -29,3 +41,14 @@ def test_device_detail_view_not_found(create_admin_user):
     client.login(username="novartis-admin", password="password")
     response = client.get('/device/100/')
     assert response.status_code == 404
+
+
+def test_device_detail_view_no_permission(create_admin_user):
+    create_admin_user()
+    customer = mixer.blend('inventory.Customer')
+    device = mixer.blend('inventory.Device', customer=customer)
+    client = Client()
+    client.login(username="novartis-admin", password="password")
+    response = client.get('/device/' + str(device.id) + '/')
+    assert response.status_code == 403
+
