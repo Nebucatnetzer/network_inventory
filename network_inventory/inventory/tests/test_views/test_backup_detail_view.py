117fbaf0849ef00d73e4b1598a63792d5890225e
Andreas Zweili <andreas@2li.ch>
2019-12-29 14:03:09 +0100

    test for 404 error for non existent backup

--- network_inventory/inventory/tests/test_views/test_backup_detail_view.py
+++ network_inventory/inventory/tests/test_views/test_backup_detail_view.py
@@ -29,7 +29,7 @@ def test_backup_detail_view_not_found(create_admin_user):
     client = Client()
     client.login(username="novartis-admin", password="password")
     response = client.get('/backup/100/')
-    assert response.status_code == 200
+    assert response.status_code == 404
 
 
 def test_customer_computer_table_no_permission(create_admin_user):
