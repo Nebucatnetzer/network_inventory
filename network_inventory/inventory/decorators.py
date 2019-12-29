1b9f46189572c031a0463fa6ab6dd28e9d554b57
Andreas Zweili <andreas@2li.ch>
2019-12-29 14:03:31 +0100

    add backup_view_permission decorator

--- network_inventory/inventory/decorators.py
+++ network_inventory/inventory/decorators.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponseForbidden
 from django.shortcuts import get_object_or_404
-from .models import Computer, Customer, Device, ConnectedDevice, Net
+from .models import Backup, Computer, Customer, Device, ConnectedDevice, Net
 
 
 def computer_view_permission(old_fuction):
@@ -66,6 +66,13 @@ def net_view_permission(old_fuction):
                 "You're not allowed to access this device."
                 )
     return new_function
+
+
+def backup_view_permission(old_fuction):
+    def new_function(request, pk, *args, **kwargs):
+        backup = get_object_or_404(Backup, pk=pk)
+        user = request.user
+        if user.has_perm('inventory.view_customer', backup.computer.customer):
             return old_fuction(request, pk)
         else:
             return HttpResponseForbidden(
