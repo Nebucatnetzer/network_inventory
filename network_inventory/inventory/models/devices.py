c885ab4ac747c2cf5ec6dd698bf3545c8fb8e960
Andreas Zweili <andreas@2li.ch>
2019-12-29 19:04:21 +0100

    delete device when customer gets deleted

--- network_inventory/inventory/models/devices.py
+++ network_inventory/inventory/models/devices.py
@@ -18,8 +18,8 @@ class Device(models.Model):
     serialnumber = models.CharField(max_length=50, blank=True)
     category = models.ForeignKey(DeviceCategory, on_delete=models.SET_NULL,
                                  null=True, blank=True)
-    customer = models.ForeignKey(Customer)
     owner = models.ForeignKey(Owner, on_delete=models.SET_NULL, null=True, blank=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     manufacturer = models.ForeignKey(DeviceManufacturer, models.SET_NULL,
                                      null=True, blank=True)
     location = models.ForeignKey(Location, models.SET_NULL, null=True,
