1e406621ca7a1d0025c1ddc4669067e24efb5e51
Andreas Zweili <andreas@2li.ch>
2020-01-01 19:53:49 +0100

    remove order link from customer table

--- network_inventory/inventory/tables.py
+++ network_inventory/inventory/tables.py
@@ -12,12 +12,18 @@ from .models import UserLicense
 
 class CustomersTable(tables.Table):
     name = tables.LinkColumn('customer', args=[A('pk')])
-    nets = tables.LinkColumn('nets', text='Nets', args=[A('pk')])
-    computers = tables.LinkColumn('computers', text='Computers', args=[A('pk')])
-    devices = tables.LinkColumn('devices', text='Devices', args=[A('pk')])
-    backups = tables.LinkColumn('backups', text='Backups', args=[A('pk')])
-    licenses = tables.LinkColumn('licenses', text='Licenses', args=[A('pk')])
-    users = tables.LinkColumn('users', text='Users', args=[A('pk')])
+    nets = tables.LinkColumn('nets', text='Nets', args=[A('pk')],
+                             orderable=False)
+    computers = tables.LinkColumn('computers', text='Computers', args=[A('pk')],
+                                  orderable=False)
+    devices = tables.LinkColumn('devices', text='Devices', args=[A('pk')],
+                                orderable=False)
+    backups = tables.LinkColumn('backups', text='Backups', args=[A('pk')],
+                                orderable=False)
+    licenses = tables.LinkColumn('licenses', text='Licenses', args=[A('pk')],
+                                 orderable=False)
+    users = tables.LinkColumn('users', text='Users', args=[A('pk')],
+                              orderable=False)
 
     class Meta:
         template_name = 'django_tables2/semantic.html'
