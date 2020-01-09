8e9ca5fafecaffb386ecb421165a7ec4c5fc292b
Andreas Zweili <andreas@2li.ch>
2020-01-09 22:01:32 +0100

    remove the user and group models from the inventory app

--- inventory/models/__init__.py
+++ inventory/models/__init__.py
@@ -3,13 +3,9 @@ from .computer import (Computer, ComputerCpuRelation, ComputerDiskRelation,
                        ComputerRamRelation,
                        ComputerSoftwareRelation)
 from .cpu import CpuArchitecture, CpuManufacturer, Cpu
-from .devices import (DeviceCategory, Device, ConnectedDevice, DeviceInNet,
-                      DeviceManufacturer)
 from .disk import DiskType, Disk
-from .groups import Group, AdGroup, MailGroup
 from .license import (License, ComputerLicense, UserLicense,
                       LicenseWithComputer, LicenseWithUser)
-from .location import Location
 from .mailalias import MailAlias
 from .net import Net, IpStatus
 from .notification import Notification, NotificationType
@@ -17,5 +13,4 @@ from .os import OperatingSystem
 from .raid import DisksInRaid, RaidType, Raid
 from .ram import RamType, Ram
 from .software import SoftwareArchitecture, SoftwareCategory, Software
-from .user import User, UserInAdGroup, UserInMailGroup
 from .warranty import Warranty, WarrantyType
