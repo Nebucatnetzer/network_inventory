e7ac8d95cc624242b17b85cea5532066490a0350
Andreas Zweili <andreas@2li.ch>
2020-01-11 17:14:34 +0100

    remove BackupTable from inventory app

--- inventory/tables.py
+++ inventory/tables.py
@@ -1,6 +1,5 @@
 import django_tables2 as tables
 
-from .models import Backup
 from .models import ComputerLicense
 from .models import UserLicense
 
@@ -18,17 +17,6 @@ class ComputersTable(tables.Table):
 
     class Meta:
         template_name = 'django_tables2/semantic.html'
-
-
-class BackupsTable(tables.Table):
-    id = tables.Column(visible=False)
-    name = tables.Column('Backup', linkify=True)
-    computer = tables.Column('Computer', linkify=True)
-    target_device = tables.ManyToManyColumn(linkify_item=True)
-
-    class Meta:
-        template_name = 'django_tables2/semantic.html'
-        model = Backup
 
 
 class UserLicensesTable(tables.Table):
