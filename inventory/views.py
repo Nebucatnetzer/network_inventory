74c1d202988ecf1ed2eb6a8ba24ebd30c670ed6f
Andreas Zweili <andreas@2li.ch>
2020-01-11 18:18:23 +0100

    move license_table_view to softwares app

--- inventory/views.py
+++ inventory/views.py
@@ -74,17 +74,3 @@ class ComputersFilterView(LoginRequiredMixin, SingleTableMixin, FilterView):
                                          klass=Customer)
         results = Computer.objects.filter(customer__in=customers)
         return results
-
-
-@login_required
-@customer_view_permission
-def licenses_table_view(request, pk):
-    user_licenses = UserLicensesTable(UserLicense.objects.filter(customer=pk))
-    computer_licenses = ComputerLicensesTable(
-        ComputerLicense.objects.filter(customer=pk))
-    RequestConfig(request).configure(user_licenses)
-    RequestConfig(request).configure(computer_licenses)
-    return render(request,
-                  'inventory/license_list.html',
-                  {'user_licenses': user_licenses,
-                   'computer_licenses': computer_licenses})
--- softwares/views.py
+++ softwares/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+@login_required
+@customer_view_permission
+def licenses_table_view(request, pk):
+    user_licenses = UserLicensesTable(UserLicense.objects.filter(customer=pk))
+    computer_licenses = ComputerLicensesTable(
+        ComputerLicense.objects.filter(customer=pk))
+    RequestConfig(request).configure(user_licenses)
+    RequestConfig(request).configure(computer_licenses)
+    return render(request,
+                  'inventory/license_list.html',
+                  {'user_licenses': user_licenses,
+                   'computer_licenses': computer_licenses})
