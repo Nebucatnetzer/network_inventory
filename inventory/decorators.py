5828347746d6c957c5b75da8913335d2c9d1477b
Andreas Zweili <andreas@2li.ch>
2020-01-11 17:13:43 +0100

    remove backup_view_permission decorator from inventory app

--- inventory/decorators.py
+++ inventory/decorators.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponseForbidden
 from django.shortcuts import get_object_or_404
 
-from .models import Backup
 from .models import Computer
 
 
@@ -10,19 +9,6 @@ def computer_view_permission(old_fuction):
         computer = get_object_or_404(Computer, pk=pk)
         user = request.user
         if user.has_perm('customers.view_customer', computer.customer):
-            return old_fuction(request, pk)
-        else:
-            return HttpResponseForbidden(
-                "You're not allowed to access this device."
-                )
-    return new_function
-
-
-def backup_view_permission(old_fuction):
-    def new_function(request, pk, *args, **kwargs):
-        backup = get_object_or_404(Backup, pk=pk)
-        user = request.user
-        if user.has_perm('customers.view_customer', backup.computer.customer):
             return old_fuction(request, pk)
         else:
             return HttpResponseForbidden(
