dc56282bdccd26947fe5c7be68ea4bc868bad0eb
Andreas Zweili <andreas@2li.ch>
2020-01-11 18:23:29 +0100

    correct the template path for lisence_list.html

--- softwares/views.py
+++ softwares/views.py
@@ -21,6 +21,6 @@ def licenses_table_view(request, pk):
     RequestConfig(request).configure(user_licenses)
     RequestConfig(request).configure(computer_licenses)
     return render(request,
-                  'inventory/license_list.html',
+                  'softwares/license_list.html',
                   {'user_licenses': user_licenses,
                    'computer_licenses': computer_licenses})
