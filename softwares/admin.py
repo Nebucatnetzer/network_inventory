faf463e7d9d83c447d8d094d9aa1581f48f74e95
Andreas Zweili <andreas@2li.ch>
2020-01-12 12:46:49 +0100

    remove license models from softwares admin

--- softwares/admin.py
+++ softwares/admin.py
@@ -3,22 +3,38 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import (
-    ComputerLicense,
-    UserLicense,
+    OperatingSystem,
+    Software,
+    SoftwareArchitecture,
+    SoftwareCategory,
 )
 
 
-class UserLicenseAdmin(admin.ModelAdmin):
-    model = UserLicense
-    extra = 0
-    verbose_name_plural = 'User Licenses'
+class OperatingSystemAdmin(admin.ModelAdmin):
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}
 
 
-class ComputerLicenseAdmin(admin.ModelAdmin):
-    model = ComputerLicense
-    extra = 0
-    verbose_name_plural = 'Computer Licenses'
+class SoftwareArchitectureAdmin(admin.ModelAdmin):
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}
 
 
-admin.site.register(ComputerLicense, ComputerLicenseAdmin)
-admin.site.register(UserLicense, UserLicenseAdmin)
+class SoftwareCategoryAdmin(admin.ModelAdmin):
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}
+
+
+admin.site.register(OperatingSystem, OperatingSystemAdmin)
+admin.site.register(Software)
+admin.site.register(SoftwareArchitecture, SoftwareArchitectureAdmin)
+admin.site.register(SoftwareCategory, SoftwareCategoryAdmin)
