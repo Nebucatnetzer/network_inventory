4f830a2f2fa12c5050a9be8b60c77ebf4e70222f
Andreas Zweili <andreas@2li.ch>
2020-01-12 12:42:43 +0100

    remove software and license imports

--- inventory/models/__init__.py
+++ inventory/models/__init__.py
@@ -3,9 +3,6 @@ from .computer import (Computer, ComputerCpuRelation, ComputerDiskRelation,
                        ComputerSoftwareRelation)
 from .cpu import CpuArchitecture, CpuManufacturer, Cpu
 from .disk import DiskType, Disk
-from .license import (License, ComputerLicense, UserLicense,
-                      LicenseWithComputer, LicenseWithUser)
 from .raid import DisksInRaid, RaidType, Raid
 from .ram import RamType, Ram
-from .software import SoftwareArchitecture, SoftwareCategory, Software
 from .warranty import Warranty, WarrantyType
