f042d4fd74b0784f0b317299312a56919ea12583
Andreas Zweili <andreas@2li.ch>
2020-02-16 17:01:55 +0100

    remove duplicate test
    since I move the device details to a separate template block I only need the
    test in one place.

--- devices/tests/test_connected_device_detail_view.py
+++ devices/tests/test_connected_device_detail_view.py
@@ -13,35 +13,6 @@ def test_connected_device_detail_view_not_logged_in():
     assert response.status_code == 302 and 'login' in response.url
 
 
-def test_connected_device_detail_view(create_admin_user):
-    fixture = create_admin_user()
-    mixer.blend('devices.DeviceCategory')
-    mixer.blend('devices.HardwareModel')
-    mixer.blend('customers.Owner')
-    mixer.blend('customers.Location')
-    connected_device = mixer.blend('devices.ConnectedDevice',
-                                   customer=fixture['customer'],
-                                   owner=mixer.SELECT,
-                                   category=mixer.SELECT,
-                                   manufacturer=mixer.SELECT,
-                                   hardware_model=mixer.SELECT,
-                                   location=mixer.SELECT)
-    client = Client()
-    client.login(username="novartis-admin", password="password")
-    response = client.get('/connected_device/'
-                          + str(connected_device.id)
-                          + '/')
-    assert (response.status_code == 200
-            and helper.in_content(response, connected_device)
-            and helper.in_content(response, connected_device.serialnumber)
-            and helper.in_content(response, connected_device.category)
-            and helper.in_content(response, connected_device.owner)
-            and helper.in_content(response, connected_device.customer)
-            and helper.in_content(response, connected_device.manufacturer)
-            and helper.in_content(response, connected_device.hardware_model)
-            and helper.in_content(response, connected_device.location))
-
-
 def test_connected_device_detail_view_not_found(create_admin_user):
     create_admin_user()
     client = Client()
