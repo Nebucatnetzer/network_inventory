1f4c3f997dd6dff7e670dd0fc2e551c5496a767a
Andreas Zweili <andreas@2li.ch>
2020-02-16 17:03:53 +0100

    add an additional network to a connected_device test

--- devices/tests/test_connected_device_detail_view.py
+++ devices/tests/test_connected_device_detail_view.py
@@ -37,13 +37,19 @@ def test_connected_device_detail_view_no_permission(create_admin_user):
 def test_connected_device_detail_view_net_relation(create_admin_user):
     create_admin_user()
     device = mixer.blend('devices.ConnectedDevice', customer=mixer.SELECT)
-    net = mixer.blend('nets.Net', customer=mixer.SELECT)
-    device_in_net = mixer.blend('devices.DeviceInNet',
-                                device=device,
-                                net=net,
-                                ip="10.7.89.100")
+    net1 = mixer.blend('nets.Net', customer=mixer.SELECT)
+    net2 = mixer.blend('nets.Net', customer=mixer.SELECT)
+    device_in_net1 = mixer.blend('devices.DeviceInNet',
+                                 device=device,
+                                 net=net1,
+                                 ip="10.7.89.100")
+    device_in_net2 = mixer.blend('devices.DeviceInNet',
+                                 device=device,
+                                 net=net2,
+                                 ip="10.8.89.100")
     client = Client()
     client.login(username="novartis-admin", password="password")
     response = client.get('/connected_device/' + str(device.id) + '/')
     assert (response.status_code == 200
-            and helper.in_content(response, device_in_net.ip))
+            and helper.in_content(response, device_in_net1.ip)
+            and helper.in_content(response, device_in_net2.ip))
