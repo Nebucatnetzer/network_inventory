25b2c3c02d70fac114660a8709f619d75272d349
Andreas Zweili <andreas@2li.ch>
2020-02-29 19:25:00 +0100

    Use a better string for the wrong model name

--- core/tests/test_utils.py
+++ core/tests/test_utils.py
@@ -9,4 +9,4 @@ pytestmark = pytest.mark.django_db
 def test_get_objects_raises_404(create_admin_user):
     fixture = create_admin_user()
     with pytest.raises(Http404):
-        utils.get_objects("Users", fixture['admin'])
+        utils.get_objects("WrongModelName", fixture['admin'])
