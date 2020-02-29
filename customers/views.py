09d3e522ac5a8e21782e8e39b31e4ba0020b8143
Andreas Zweili <andreas@2li.ch>
2020-02-29 15:48:29 +0100

    implemente the utils.get_customers function

--- customers/views.py
+++ customers/views.py
@@ -5,18 +5,15 @@ from django.views.generic import DetailView
 
 from django_tables2 import RequestConfig
 from guardian.mixins import PermissionRequiredMixin
-from guardian.shortcuts import get_objects_for_user
 
+from core import utils
 from .models import Customer
 from .tables import CustomersTable
 
 
 @login_required
 def customers_table_view(request):
-    table = CustomersTable(
-        get_objects_for_user(request.user,
-                             'customers.view_customer',
-                             klass=Customer))
+    table = CustomersTable(utils.get_customers(request.user))
     RequestConfig(request).configure(table)
     return render(request, 'customers/customer_list.html', {'customers': table})
 
