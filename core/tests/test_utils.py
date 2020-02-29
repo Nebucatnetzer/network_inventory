ceb1aa59cc5b7085e95182b6e9e17c218d60afdf
Andreas Zweili <andreas@2li.ch>
2020-02-29 18:54:04 +0100

    add test_get_objects_raises_404

---
+++ core/tests/test_utils.py
@@ -0,0 +1,12 @@
+from django.http import Http404
+import pytest
+
+from core import utils
+
+pytestmark = pytest.mark.django_db
+
+
+def test_get_objects_raises_404(create_admin_user):
+    fixture = create_admin_user()
+    with pytest.raises(Http404):
+        utils.get_objects("Users", fixture['admin'])
