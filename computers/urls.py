1b779482be9131dd3c7fd174127f99257c9477c3
Andreas Zweili <andreas@2li.ch>
2020-02-29 21:05:37 +0100

    split parameters over multiple lines

--- computers/urls.py
+++ computers/urls.py
@@ -4,13 +4,18 @@ from . import views
 
 urlpatterns = [
     path('customer/<int:pk>/computers/',
-         views.computers_table_view, name='computers'),
-    path('computer/<int:pk>/', views.computer_detail_view,
+         views.computers_table_view,
+         name='computers'),
+    path('computer/<int:pk>/',
+         views.computer_detail_view,
          name='computer'),
-    path('computers/all/', views.ComputersFilterView.as_view(),
+    path('computers/all/',
+         views.ComputersFilterView.as_view(),
          name='all_computers'),
-    path('customer/<int:pk>/create/computer/', views.ComputerCreateFromCustomerView.as_view(),
+    path('customer/<int:pk>/create/computer/',
+         views.ComputerCreateFromCustomerView.as_view(),
          name='computer_create'),
-    path('update/computer/<int:pk>/', views.ComputerUpdateView.as_view(),
+    path('update/computer/<int:pk>/',
+         views.ComputerUpdateView.as_view(),
          name='computer_update'),
  ]
