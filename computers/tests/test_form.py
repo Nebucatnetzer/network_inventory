8eb77ba0c57e681bd88036255c48ea5a6bfcb38f
Andreas Zweili <andreas@2li.ch>
2020-02-29 21:11:23 +0100

    add test for ComputerCreateForm

---
+++ computers/tests/test_form.py
@@ -0,0 +1,23 @@
+import pytest
+
+from computers import forms
+
+pytestmark = pytest.mark.django_db
+
+
+def test_computer_create_form(create_admin_user):
+    fixture = create_admin_user()
+    form = forms.ComputerCreateForm(user=fixture['admin'], data={})
+    assert form.is_valid() is False, (
+        "Should be false because no data was given")
+
+    data = {"name": "novartis-pc1",
+            "customer": 3}
+    form = forms.ComputerCreateForm(user=fixture['admin'], data=data)
+    assert form.is_valid() is False, (
+        "Should be false because the customer doesn't exist.")
+
+    data = {"name": "novartis-pc1",
+            "customer": fixture['customer'].id}
+    form = forms.ComputerCreateForm(user=fixture['admin'], data=data)
+    assert form.is_valid() is True, ("Should be valid with the given data.")
