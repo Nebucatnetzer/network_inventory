0cb8023204b4f073ca748fe980bfb2d470f3ad44
Andreas Zweili <andreas@2li.ch>
2020-02-29 21:07:22 +0100

    split paremeters over multiple lines

--- customers/tables.py
+++ customers/tables.py
@@ -5,22 +5,35 @@ from core.tables import CoreTable
 
 
 class CustomersTable(CoreTable):
-    name = tables.LinkColumn('customer', args=[A('pk')])
-    nets = tables.LinkColumn('nets', text='Nets', args=[A('pk')],
+    name = tables.LinkColumn('customer',
+                             args=[A('pk')])
+    nets = tables.LinkColumn('nets',
+                             text='Nets',
+                             args=[A('pk')],
                              orderable=False)
-    computers = tables.LinkColumn('computers', text='Computers', args=[A('pk')],
+    computers = tables.LinkColumn('computers',
+                                  text='Computers',
+                                  args=[A('pk')],
                                   orderable=False)
     connected_devices = tables.LinkColumn('connected_devices',
                                           text='Connected Devices',
                                           args=[A('pk')],
                                           orderable=False)
-    devices = tables.LinkColumn('devices', text='Devices', args=[A('pk')],
+    devices = tables.LinkColumn('devices',
+                                text='Devices',
+                                args=[A('pk')],
                                 orderable=False)
-    backups = tables.LinkColumn('backups', text='Backups', args=[A('pk')],
+    backups = tables.LinkColumn('backups',
+                                text='Backups',
+                                args=[A('pk')],
                                 orderable=False)
-    licenses = tables.LinkColumn('licenses', text='Licenses', args=[A('pk')],
+    licenses = tables.LinkColumn('licenses',
+                                 text='Licenses',
+                                 args=[A('pk')],
                                  orderable=False)
-    users = tables.LinkColumn('users', text='Users', args=[A('pk')],
+    users = tables.LinkColumn('users',
+                              text='Users',
+                              args=[A('pk')],
                               orderable=False)
 
     class Meta(CoreTable.Meta):
