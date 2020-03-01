b2bc1f5d88c24b62cfce5caa99c633f7d6e40a6a
Andreas Zweili <andreas@2li.ch>
2020-03-01 11:16:36 +0100

    change the customer property to a ForeignKey
    Everytime the warranty gets saved the customer gets
    set to the one from the corresponding device

--- devices/models/warranty.py
+++ devices/models/warranty.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from core.models import Category
 from core.utils import td_format
+from customers.models import Customer
 
 from .device import Device
 
@@ -14,21 +15,26 @@ class WarrantyType(Category):
 
 
 class Warranty(models.Model):
+    customer = models.ForeignKey(Customer,
+                                 on_delete=models.CASCADE,
+                                 blank=True)
     device = models.ForeignKey(Device, on_delete=models.CASCADE)
     valid_from = models.DateField()
     valid_until = models.DateField()
-    warranty_type = models.ForeignKey(WarrantyType, models.SET_NULL,
-                                      blank=True, null=True)
+    warranty_type = models.ForeignKey(WarrantyType,
+                                      models.SET_NULL,
+                                      blank=True,
+                                      null=True)
+
+    def save(self, *args, **kwargs):
+        self.customer = self.device.customer
+        super(Warranty, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.device)
 
     class Meta:
         verbose_name_plural = "Warranties"
-
-    @property
-    def customer(self):
-        return self.device.customer
 
     @property
     def duration_in_years(self):
