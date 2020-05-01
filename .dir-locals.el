46fe940226f8a509759936b282fa8ea6dad773f0
Andreas Zweili <andreas@2li.ch>
2020-05-01 12:49:05 +0200

    add the pytest options to the dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,4 +4,5 @@
 ((python-mode
   (pyvenv-activate . "~/git_repos/projects/network_inventory/venv/")
   (eval progn
-        (setenv "DJANGO_SETTINGS_MODULE" "network_inventory.settings.local"))))
+        (setenv "DJANGO_SETTINGS_MODULE" "network_inventory.settings.local")
+        (setenv "PYTEST_ADDOPTS" "-n 4 --nomigrations"))))
