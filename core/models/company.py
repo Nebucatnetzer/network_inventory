459771924c9dd8aa32ae9000c85840359b49acf8
Andreas Zweili <andreas@2li.ch>
2020-06-08 21:07:40 +0200

    move the description field to the base Company model

--- core/models/company.py
+++ core/models/company.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
 
     class Meta:
         abstract = True
--- customers/models.py
+++ customers/models.py
@@ -8,7 +8,6 @@ class Owner(Company):
 
 class Customer(Company):
     name = models.CharField(max_length=50)
-    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
