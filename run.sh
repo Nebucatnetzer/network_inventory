e68d3b879de3eeaf8a242741c2bdc51c465f8e97
Andreas Zweili <andreas@2li.ch>
2020-07-04 11:54:07 +0200

    reload unicorn when code changes

--- run.sh
+++ run.sh
@@ -26,4 +26,4 @@ else
     python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')"
     touch .second_run
 fi
-gunicorn network_inventory.wsgi:application --bind 0.0.0.0:8000 --workers 3
+gunicorn network_inventory.wsgi:application --reload --bind 0.0.0.0:8000 --workers 3
