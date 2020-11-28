5e9edcd9682b767f2f142ccba582525fc60c9cd4
Andreas Zweili <andreas@2li.ch>
2020-11-28 16:20:16 +0100

    add tests for month and dayofmonth

--- backend/core/tests/test_rest.py
+++ backend/core/tests/test_rest.py
@@ -4,7 +4,21 @@ from django.urls import reverse
 
 
 @pytest.mark.django_db
-def test_unauthorized_request(api_client):
+def test_unauthorized_request_weekday(api_client):
     url = reverse('weekday-list')
+    response = api_client.get(url)
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_unauthorized_request_month(api_client):
+    url = reverse('month-list')
+    response = api_client.get(url)
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_unauthorized_request_day_of_month(api_client):
+    url = reverse('dayofmonth-list')
     response = api_client.get(url)
     assert response.status_code == 403
