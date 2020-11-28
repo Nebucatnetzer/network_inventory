cc68c5a5859095f2bce2c9a503b7dd98e997cf8a
Andreas Zweili <andreas@2li.ch>
2020-11-28 16:28:38 +0100

    add tests for HoursInDay and MinutesInHour

--- backend/core/tests/test_views.py
+++ backend/core/tests/test_views.py
@@ -22,3 +22,17 @@ def test_unauthorized_request_day_of_month(api_client):
     url = reverse('dayofmonth-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_unauthorized_request_hours_in_day(api_client):
+    url = reverse('hoursinday-list')
+    response = api_client.get(url)
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_unauthorized_request_minutes_in_hour(api_client):
+    url = reverse('minutesinhour-list')
+    response = api_client.get(url)
+    assert response.status_code == 403
