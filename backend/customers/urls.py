a2cb35ceaa5cf54d2650820a1980d06dfde29f91
Andreas Zweili <andreas@2li.ch>
2020-11-30 21:29:26 +0100

    add views for customer rest api

--- backend/customers/urls.py
+++ backend/customers/urls.py
@@ -2,8 +2,16 @@ from django.urls import path, include
 
 from rest_framework import routers
 
+from . import views
+
 
 router = routers.DefaultRouter()
+router.register(r'owners', views.OwnerViewSet)
+router.register(r'customers', views.CustomerViewSet)
+router.register(r'device-manufacturers',
+                views.DeviceManufacturerViewSet,
+                'device-manufacturer')
+router.register(r'locations', views.LocationViewSet)
 
 
 urlpatterns = [
---
+++ backend/customers/views.py
@@ -0,0 +1,48 @@
+from rest_framework import viewsets
+from rest_framework import permissions
+
+from .models import Owner
+from .models import Customer
+from .models import DeviceManufacturer
+from .models import Location
+
+from .serializers import OwnerSerializer
+from .serializers import CustomerSerializer
+from .serializers import DeviceManufacturerSerializer
+from .serializers import LocationSerializer
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows weekdays to be viewed or edited.
+    """
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows weekdays to be viewed or edited.
+    """
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class DeviceManufacturerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows weekdays to be viewed or edited.
+    """
+    queryset = DeviceManufacturer.objects.all()
+    serializer_class = DeviceManufacturerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class LocationViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows weekdays to be viewed or edited.
+    """
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    permission_classes = [permissions.IsAuthenticated]
