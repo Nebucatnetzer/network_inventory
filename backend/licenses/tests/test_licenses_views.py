3abea82b11ed28b495e811e90ec1d357e92c0798
Andreas Zweili <andreas@2li.ch>
2020-12-07 16:25:33 +0100

    add authenticated tests for license views

--- backend/licenses/tests/test_licenses_views.py
+++ backend/licenses/tests/test_licenses_views.py
@@ -11,10 +11,24 @@ def test_unauthorized_request_user_license(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_user_license(api_client_authenticated):
+    url = reverse('userlicense-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_computer_license(api_client):
     url = reverse('computerlicense-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_computer_license(api_client_authenticated):
+    url = reverse('computerlicense-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -25,7 +39,21 @@ def test_unauthorized_request_license_with_user(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_license_with_user(api_client_authenticated):
+    url = reverse('licensewithuser-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_license_with_computer(api_client):
     url = reverse('licensewithcomputer-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_license_with_computer(api_client_authenticated):
+    url = reverse('licensewithcomputer-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
