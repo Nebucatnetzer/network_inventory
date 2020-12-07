6078f0105acda2d39f9307bc21d16b1ef85f11ca
Andreas Zweili <andreas@2li.ch>
2020-12-07 16:25:14 +0100

    add authenticated tests for device views

--- backend/devices/tests/test_devices_views.py
+++ backend/devices/tests/test_devices_views.py
@@ -11,10 +11,10 @@ def test_unauthorized_request_warranty(api_client):
 
 
 @pytest.mark.django_db
-def test_unauthorized_request_warranty_type(api_client):
+def test_authorized_request_warranty_type(api_client_authenticated):
     url = reverse('warrantytype-list')
-    response = api_client.get(url)
-    assert response.status_code == 403
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -25,10 +25,24 @@ def test_unauthorized_request_device(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_device(api_client_authenticated):
+    url = reverse('device-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_device_category(api_client):
     url = reverse('devicecategory-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_device_category(api_client_authenticated):
+    url = reverse('devicecategory-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -39,6 +53,13 @@ def test_unauthorized_request_device_in_net(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_device_in_net(api_client_authenticated):
+    url = reverse('deviceinnet-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_device_manufacturer(api_client):
     url = reverse('devicemanufacturer-list')
     response = api_client.get(url)
@@ -46,7 +67,21 @@ def test_unauthorized_request_device_manufacturer(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_device_manufacturer(api_client_authenticated):
+    url = reverse('devicemanufacturer-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_hardware_model(api_client):
     url = reverse('hardwaremodel-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_hardware_model(api_client_authenticated):
+    url = reverse('hardwaremodel-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
