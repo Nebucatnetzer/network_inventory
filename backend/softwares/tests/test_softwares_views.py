65197cd939af60d006fd5ab70f094052d54176ad
Andreas Zweili <andreas@2li.ch>
2020-12-07 16:29:43 +0100

    add authenticated tests for software views

--- backend/softwares/tests/test_softwares_views.py
+++ backend/softwares/tests/test_softwares_views.py
@@ -11,10 +11,24 @@ def test_unauthorized_request_operating_system(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_operating_system(api_client_authenticated):
+    url = reverse('operatingsystem-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_software_architecture(api_client):
     url = reverse('softwarearchitecture-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_software_architecture(api_client_authenticated):
+    url = reverse('softwarearchitecture-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -25,7 +39,21 @@ def test_unauthorized_request_software_category(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_software_category(api_client_authenticated):
+    url = reverse('softwarecategory-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_software(api_client):
     url = reverse('software-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_software(api_client_authenticated):
+    url = reverse('software-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
