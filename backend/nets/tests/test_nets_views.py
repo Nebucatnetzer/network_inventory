2ca9893d95fb958067a39c847c4595e42efe3c3f
Andreas Zweili <andreas@2li.ch>
2020-12-07 16:27:13 +0100

    add authenticated tests for net views

--- backend/nets/tests/test_nets_views.py
+++ backend/nets/tests/test_nets_views.py
@@ -11,8 +11,21 @@ def test_unauthorized_request_ip_status(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_ip_status(api_client_authenticated):
+    url = reverse('ipstatus-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_net(api_client):
     url = reverse('net-list')
     response = api_client.get(url)
     assert response.status_code == 403
 
+
+@pytest.mark.django_db
+def test_authorized_request_net(api_client_authenticated):
+    url = reverse('net-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
