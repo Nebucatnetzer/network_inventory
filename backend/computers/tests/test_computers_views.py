0834f38046a12aaca63c1cac5c9ae49ed5b508f3
Andreas Zweili <andreas@2li.ch>
2020-12-07 15:57:23 +0100

    add tests for authenticated comptuer views

--- backend/computers/tests/test_computers_views.py
+++ backend/computers/tests/test_computers_views.py
@@ -11,10 +11,24 @@ def test_unauthorized_request_computer(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_computer(api_client_authenticated):
+    url = reverse('computer-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_computer_cpu_relation(api_client):
     url = reverse('computercpurelation-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_computer_cpu_relation(api_client_authenticated):
+    url = reverse('computercpurelation-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -25,10 +39,24 @@ def test_unauthorized_request_computer_disk_relation(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_computer_disk_relation(api_client_authenticated):
+    url = reverse('computerdiskrelation-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_computer_gpu_relation(api_client):
     url = reverse('computergpurelation-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_computer_gpu_relation(api_client_authenticated):
+    url = reverse('computergpurelation-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -39,10 +67,26 @@ def test_unauthorized_request_computer_ram_relation(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_computer_ram_relation(api_client_authenticated):
+    url = reverse('computerramrelation-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_computer_software_relation(api_client):
     url = reverse('computersoftwarerelation-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_computer_software_relation(
+        api_client_authenticated):
+
+    url = reverse('computersoftwarerelation-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -53,10 +97,24 @@ def test_unauthorized_request_cpu_architecture(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_cpu_architecture(api_client_authenticated):
+    url = reverse('cpuarchitecture-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_cpu_manufacturer(api_client):
     url = reverse('cpumanufacturer-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_cpu_manufacturer(api_client_authenticated):
+    url = reverse('cpumanufacturer-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -67,10 +125,24 @@ def test_unauthorized_request_cpu(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_cpu(api_client_authenticated):
+    url = reverse('cpu-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_disk_type(api_client):
     url = reverse('disktype-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_disk_type(api_client_authenticated):
+    url = reverse('disktype-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -81,10 +153,24 @@ def test_unauthorized_request_disk(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_disk(api_client_authenticated):
+    url = reverse('disk-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_gpu_manufacturer(api_client):
     url = reverse('gpumanufacturer-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_gpu_manufacturer(api_client_authenticated):
+    url = reverse('gpumanufacturer-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -95,10 +181,24 @@ def test_unauthorized_request_gpu(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_gpu(api_client_authenticated):
+    url = reverse('gpu-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_disks_in_raid(api_client):
     url = reverse('disksinraid-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_disks_in_raid(api_client_authenticated):
+    url = reverse('disksinraid-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -109,10 +209,24 @@ def test_unauthorized_request_raid_type(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_raid_type(api_client_authenticated):
+    url = reverse('raidtype-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_raid(api_client):
     url = reverse('raid-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_raid(api_client_authenticated):
+    url = reverse('raid-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -123,7 +237,21 @@ def test_unauthorized_request_ram_type(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_ram_type(api_client_authenticated):
+    url = reverse('ramtype-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_ram(api_client):
     url = reverse('ram-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_ram(api_client_authenticated):
+    url = reverse('ram-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
