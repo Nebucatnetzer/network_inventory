42a47e72c6d2a040b17f8ebe1f018a9c4b664635
Andreas Zweili <andreas@2li.ch>
2020-12-07 16:03:09 +0100

    add authenticated tests for core views

--- backend/core/tests/test_core_views.py
+++ backend/core/tests/test_core_views.py
@@ -11,10 +11,24 @@ def test_unauthorized_request_weekday(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_weekday(api_client_authenticated):
+    url = reverse('weekday-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_month(api_client):
     url = reverse('month-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_month(api_client_authenticated):
+    url = reverse('month-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -25,6 +39,13 @@ def test_unauthorized_request_day_of_month(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_day_of_month(api_client_authenticated):
+    url = reverse('dayofmonth-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_hours_in_day(api_client):
     url = reverse('hoursinday-list')
     response = api_client.get(url)
@@ -32,7 +53,21 @@ def test_unauthorized_request_hours_in_day(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_hours_in_day(api_client_authenticated):
+    url = reverse('hoursinday-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_minutes_in_hour(api_client):
     url = reverse('minutesinhour-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_minutes_in_hour(api_client_authenticated):
+    url = reverse('minutesinhour-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
