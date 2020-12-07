4cc53a1e32bcb72637e362f883543ac56a108f34
Andreas Zweili <andreas@2li.ch>
2020-12-07 16:32:48 +0100

    add authenticated tests for user views

--- backend/users/tests/test_users_views.py
+++ backend/users/tests/test_users_views.py
@@ -11,10 +11,24 @@ def test_unauthorized_request_user(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_user(api_client_authenticated):
+    url = reverse('computer-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_user_in_ad_group(api_client):
     url = reverse('computer-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_user_in_ad_group(api_client_authenticated):
+    url = reverse('computer-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -25,10 +39,24 @@ def test_unauthorized_request_user_in_mail_group(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_user_in_mail_group(api_client_authenticated):
+    url = reverse('computer-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_ad_group(api_client):
     url = reverse('computer-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_ad_group(api_client_authenticated):
+    url = reverse('computer-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -39,7 +67,21 @@ def test_unauthorized_request_mail_group(api_client):
 
 
 @pytest.mark.django_db
+def test_authorized_request_mail_group(api_client_authenticated):
+    url = reverse('computer-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
 def test_unauthorized_request_mail_alias(api_client):
     url = reverse('computer-list')
     response = api_client.get(url)
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_authorized_request_mail_alias(api_client_authenticated):
+    url = reverse('computer-list')
+    response = api_client_authenticated.get(url)
+    assert response.status_code == 200
