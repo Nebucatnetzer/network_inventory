5374df42318ffd5fa8f33e7d09bacf6ccafaa5cb
Andreas Zweili <andreas@2li.ch>
2021-02-08 10:55:20 +0100

    backend: add a missing comma

--- backend/network_inventory/settings/base.py
+++ backend/network_inventory/settings/base.py
@@ -52,7 +52,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
