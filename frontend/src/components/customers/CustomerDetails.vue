d0b67fd28a605186ceb197f5aa60466c785b84db
Andreas Zweili <andreas@2li.ch>
2021-02-15 22:24:12 +0100

    add a CustomerDetails component

---
+++ frontend/src/components/customers/CustomerDetails.vue
@@ -0,0 +1,27 @@
+<template>
+  <dialog open>
+    <header>
+      <h1>{{ customerName }}</h1>
+    </header>
+    <p>{{ customerDescription }}</p>
+    <button @click="$emit('hide-details', true)">Return to list</button>
+  </dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    customerName: {
+      type: String,
+      required: true,
+    },
+    customerDescription: {
+      type: String,
+      required: false,
+    },
+  },
+  emits: ["hide-details"],
+};
+</script>
+
+<style></style>
