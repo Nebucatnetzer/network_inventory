218a7c1b4c373c38a27a63f88728db3209f95fb6
Andreas Zweili <andreas@2li.ch>
2021-02-15 16:55:27 +0100

    frontend move Customers to a subfolder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,32 @@
 <template>
-    <div>
-        <customer></customer>
-        <the-footer></the-footer>
-    </div>
+  <div>
+    <customer></customer>
+    <the-footer></the-footer>
+  </div>
 </template>
 
 <script>
 import TheFooter from "./components/UI/TheFooter.vue";
-import Customer from "./components/Customers.vue";
+import Customer from "./components/customers/Customers.vue";
 
 export default {
-    components: {
-        TheFooter,
-        Customer,
-    },
-    data() {
-        return {};
-    },
+  components: {
+    TheFooter,
+    Customer,
+  },
+  data() {
+    return {};
+  },
 };
 </script>
 
 <style>
 html {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
---
+++ frontend/src/components/customers/AddCustomer.vue
@@ -0,0 +1,9 @@
+<template> </template>
+
+<script>
+import getAPI from "../scripts/axios-api";
+
+export default {};
+</script>
+
+<style></style>
