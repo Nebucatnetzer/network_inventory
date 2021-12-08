0c7d1d3a308e3c28211767ee5706ec7e47258ce8
Andreas Zweili <andreas@zweili.ch>
2021-12-08 18:07:49 +0100

    add flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A Python API for various tools I use at work.";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
+    flake-utils.url = github:numtide/flake-utils;
+    mach-nix.url = "github:DavHau/mach-nix";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, mach-nix }:
+    with flake-utils.lib; eachSystem allSystems (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        devShell."${system}" = pkgs.mkShell {
+          buildInputs = [ gnumake ];
+        };
+      });
+}
