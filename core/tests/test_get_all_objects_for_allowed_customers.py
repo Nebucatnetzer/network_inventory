a1161956db655ca279c9215f64a3cf422d139031
Andreas Zweili <andreas@zweili.ch>
2021-12-26 16:10:03 +0100

    add tests for get_all_objects_for_allowed_customers

---
+++ core/tests/test_get_all_objects_for_allowed_customers.py
@@ -0,0 +1,48 @@
+from mixer.backend.django import mixer
+
+import pytest
+
+from core import utils
+
+from customers.models import Customer
+from devices.models import Device
+
+pytestmark = pytest.mark.django_db
+
+
+def test_get_all_objects_for_allowed_customers(create_admin_user):
+    fixture = create_admin_user()
+    customer = fixture['customer']
+    admin = fixture['admin']
+    objects = utils.get_all_objects_for_allowed_customers(
+        Customer, user=admin)
+    assert objects[0] == customer
+
+
+def test_get_all_objects_for_allowed_customers_device(create_admin_user):
+    fixture = create_admin_user()
+    customer = fixture['customer']
+    admin = fixture['admin']
+    device = mixer.blend(Device, customer=customer)
+    objects = utils.get_all_objects_for_allowed_customers(
+        Device, user=admin)
+    assert objects[0] == device
+
+
+def test_get_all_objects_for_unallowed_customers(create_admin_user):
+    fixture = create_admin_user()
+    customer = mixer.blend(Customer)
+    admin = fixture['admin']
+    objects = utils.get_all_objects_for_allowed_customers(
+        Customer, user=admin)
+    assert customer not in objects
+
+
+def test_get_all_objects_for_unallowed_customers_device(create_admin_user):
+    fixture = create_admin_user()
+    customer = mixer.blend(Customer)
+    admin = fixture['admin']
+    device = mixer.blend(Device, customer=customer)
+    objects = utils.get_all_objects_for_allowed_customers(
+        Device, user=admin)
+    assert device not in objects
