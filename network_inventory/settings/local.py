3cbbebb61b08ec577fbaebd2a623cdc8390feb93
Andreas Zweili <andreas@zweili.ch>
2022-02-07 22:27:59 +0100

    remove the docker development environment
    It's just easier to have the stuff in a venv

--- network_inventory/settings/local.py
+++ network_inventory/settings/local.py
@@ -5,16 +5,23 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'development_key'
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+]
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "foo"
+
 DEBUG = True
 CRISPY_FAIL_SILENTLY = not DEBUG
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'PASSWORD': 'password',
     }
 }
