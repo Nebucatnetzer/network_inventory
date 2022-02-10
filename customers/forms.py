ef174c70aa969969f17c1872a0f7fb27d3a01a39
Andreas Zweili <andreas@zweili.ch>
2022-02-10 21:40:49 +0100

    rewrite the customer forms to htmx

--- customers/forms.py
+++ customers/forms.py
@@ -1,4 +1,9 @@
 from django import forms
+from django.urls import reverse_lazy
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Button, Field
+from crispy_forms.bootstrap import FormActions
 
 from .models import Customer
 
@@ -10,3 +15,20 @@ class CustomerForm(forms.ModelForm):
             'name',
             'description'
         )
+
+    def __init__(self, *args, **kwargs):
+        super(CustomerForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.attrs = {
+            'hx-post': reverse_lazy('customer_create'),
+            'id': 'customer-form',
+        }
+        self.helper.layout = Layout(
+            FormActions(
+                Field('name'),
+                Field('description'),
+                Submit('save', 'Save'),
+                Button('cancel', 'Cancel', css_class="btn btn-secondary",
+                       onclick="closeModal()")
+            ))
