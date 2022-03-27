9559bfcf95b9748c7da7edbf672a19313a8217a5
Andreas Zweili <andreas@zweili.ch>
2022-03-27 14:45:47 +0200

    format code

--- users/views.py
+++ users/views.py
@@ -32,7 +32,7 @@ from .tables import UsersTable
 def users_table_view(request, pk):
     table = UsersTable(User.objects.filter(customer=pk))
     RequestConfig(request).configure(table)
-    return render(request, 'users/user_list.html', {'users': table})
+    return render(request, "users/user_list.html", {"users": table})
 
 
 @login_required
@@ -44,20 +44,25 @@ def user_detail_view(request, pk):
     mail_alias = MailAlias.objects.filter(user=user)
     computers = Computer.objects.filter(user=user)
     licenses = LicenseWithUser.objects.filter(user=user)
-    return render(request, 'users/user_details.html',
-                  {'user': user,
-                   'ad_groups': ad_groups,
-                   'mail_groups': mail_groups,
-                   'mail_alias': mail_alias,
-                   'computers': computers,
-                   'licenses': licenses})
+    return render(
+        request,
+        "users/user_details.html",
+        {
+            "user": user,
+            "ad_groups": ad_groups,
+            "mail_groups": mail_groups,
+            "mail_alias": mail_alias,
+            "computers": computers,
+            "licenses": licenses,
+        },
+    )
 
 
 class UserDeleteView(LoginRequiredMixin, DeleteView):
     model = User
 
     def get_success_url(self):
-        return reverse('users', args=(self.object.customer.pk,))
+        return reverse("users", args=(self.object.customer.pk,))
 
 
 @login_required
@@ -65,62 +70,66 @@ class UserDeleteView(LoginRequiredMixin, DeleteView):
 def groups_table_view(request, pk):
     customer = get_object_or_404(Customer, pk=pk)
     ad_groups_table = AdGroupsTable(
-        utils.get_objects_for_customer(AdGroup,
-                                       user=request.user,
-                                       customer_pk=pk))
+        utils.get_objects_for_customer(
+            AdGroup, user=request.user, customer_pk=pk
+        )
+    )
     RequestConfig(request).configure(ad_groups_table)
 
     mail_groups_table = MailGroupsTable(
-        utils.get_objects_for_customer(MailGroup,
-                                       user=request.user,
-                                       customer_pk=pk))
+        utils.get_objects_for_customer(
+            MailGroup, user=request.user, customer_pk=pk
+        )
+    )
     RequestConfig(request).configure(mail_groups_table)
-    return TemplateResponse(request,
-                            'groups/group_list.html',
-                            {'customer': customer.name,
-                             'ad_groups': ad_groups_table,
-                             'mail_groups': mail_groups_table})
+    return TemplateResponse(
+        request,
+        "groups/group_list.html",
+        {
+            "customer": customer.name,
+            "ad_groups": ad_groups_table,
+            "mail_groups": mail_groups_table,
+        },
+    )
 
 
 @login_required
 def ad_group_detail_view(request, pk):
-    group = utils.get_object_with_view_permission(AdGroup,
-                                                  user=request.user,
-                                                  pk=pk)
-    return render(request, 'groups/group_details.html',
-                  {'group': group})
+    group = utils.get_object_with_view_permission(
+        AdGroup, user=request.user, pk=pk
+    )
+    return render(request, "groups/group_details.html", {"group": group})
 
 
 @login_required
 def mail_group_detail_view(request, pk):
-    group = utils.get_object_with_view_permission(MailGroup,
-                                                  user=request.user,
-                                                  pk=pk)
-    return render(request, 'groups/group_details.html',
-                  {'group': group})
+    group = utils.get_object_with_view_permission(
+        MailGroup, user=request.user, pk=pk
+    )
+    return render(request, "groups/group_details.html", {"group": group})
 
 
 @login_required
 def delete_ad_group(request, pk):
-    group = utils.get_object_with_view_permission(AdGroup,
-                                                  user=request.user,
-                                                  pk=pk)
-    if request.method == 'POST':
+    group = utils.get_object_with_view_permission(
+        AdGroup, user=request.user, pk=pk
+    )
+    if request.method == "POST":
         group.delete()
-        return redirect('ad_groups', pk=group.customer.pk)
-    return TemplateResponse(request,
-                            'groups/ad_group_confirm_delete.html',
-                            {'object': group})
+        return redirect("ad_groups", pk=group.customer.pk)
+    return TemplateResponse(
+        request, "groups/ad_group_confirm_delete.html", {"object": group}
+    )
 
 
 @login_required
 def delete_mail_group(request, pk):
-    group = utils.get_object_with_view_permission(MailGroup,
-                                                  user=request.user,
-                                                  pk=pk)
-    if request.method == 'POST':
+    group = utils.get_object_with_view_permission(
+        MailGroup, user=request.user, pk=pk
+    )
+    if request.method == "POST":
         group.delete()
-        return redirect('mail_groups', pk=group.customer.pk)
-    return TemplateResponse(request,
-                            'groups/mail_group_confirm_delete.html',
-                            {'object': group})
+        return redirect("mail_groups", pk=group.customer.pk)
+    return TemplateResponse(
+        request, "groups/mail_group_confirm_delete.html", {"object": group}
+    )
