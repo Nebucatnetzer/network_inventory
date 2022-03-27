cb3900d80ca77c15074b8fb83e02db8042fe5234
Andreas Zweili <andreas@zweili.ch>
2022-03-27 14:54:43 +0200

    line length

--- devices/models/device.py
+++ devices/models/device.py
@@ -9,7 +9,10 @@ from nets.models import Net, IpStatus
 class DeviceManufacturer(Company):
     phone_regex = RegexValidator(
         regex=r"^\+?1?\d{9,15}$",
-        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.",
+        message="""
+                Phone number must be entered in the format: '+999999999'.
+                Up to 15 digits allowed.
+                """,
     )
     phone_number = models.CharField(
         validators=[phone_regex], max_length=17, blank=True, null=True
