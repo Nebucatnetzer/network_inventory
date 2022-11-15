c07bc5268b09bfcb28b2c14545d3c4e0395639fb
Andreas Zweili <andreas@zweili.ch>
2022-11-15 22:04:13 +0100

    WIP poetry

--- flake.nix
+++ flake.nix
@@ -5,53 +5,43 @@
     flake-utils = {
       url = github:numtide/flake-utils;
     };
-    pypi = {
-      url = "github:DavHau/pypi-deps-db";
-      flake = false;
-    };
-    mach-nix = {
-      inputs.pypi-deps-db.follows = "pypi";
-      url = "mach-nix/3.5.0";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
-  outputs = { self, nixpkgs, flake-utils, mach-nix, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    {
+      # Nixpkgs overlay providing the application
+      overlay = nixpkgs.lib.composeManyExtensions [
+        poetry2nix.overlay
+        (final: prev: {
+          # The application
+          network_inventory = prev.poetry2nix.mkPoetryApplication {
+            projectDir = ./.;
+          };
+        })
+      ];
+    } // (flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        machNix = mach-nix.lib."${system}";
-        devEnvironment = machNix.mkPython {
-          requirements = builtins.readFile ./requirements/local.txt;
-          _.pytest-cov.propagatedBuildInputs.mod = pySelf: self: oldVal: oldVal ++ [ pySelf.tomli ];
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
         };
       in
       {
+        apps = {
+          network_inventory = pkgs.network_inventory;
+        };
+
+        defaultApp = pkgs.network_inventory;
+
         devShell = pkgs.mkShell {
           buildInputs = [
-            devEnvironment
             pkgs.gnumake
-            pkgs.python39Packages.autopep8
-            pkgs.python39Packages.black
-            pkgs.python39Packages.flake8
-            pkgs.python39Packages.jedi
-            pkgs.python39Packages.pip
-            pkgs.python39Packages.yapf
+            pkgs.python39Packages.poetry
+            pkgs.network_inventory
           ];
-          shellHook = ''
-            export DJANGO_SETTINGS_MODULE=network_inventory.settings.local
-          '';
         };
-        packages.venv = devEnvironment;
-        defaultPackage = (machNix.mkDockerImage {
-          packagesExtra = with pkgs;
-            [ pkgs.bash ];
-          requirements = builtins.readFile ./requirements/docker.txt;
-          _.pytest-cov.propagatedBuildInputs.mod = pySelf: self: oldVal: oldVal ++ [ pySelf.tomli ];
-        }).override
-          (oldAttrs: {
-            name = "network-inventory";
-            config.Cmd = [ "run.sh" ];
-          });
-      });
+      }));
 }
