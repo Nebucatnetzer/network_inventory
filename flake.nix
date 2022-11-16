6e87a7a7da7c1b9ea04582bf66d56aa97df8a2f1
Andreas Zweili <andreas@zweili.ch>
2022-11-16 22:31:34 +0100

    Revert "Shrink the flake to a more usable size"
    This reverts commit cba8690ea98cb4971a9345d2ee04c536e64908ac.

--- flake.nix
+++ flake.nix
@@ -2,23 +2,46 @@
   description = "A Python API for various tools I use at work.";
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
-    flake-utils.url = github:numtide/flake-utils;
+    flake-utils = {
+      url = github:numtide/flake-utils;
+    };
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
+    {
+      # Nixpkgs overlay providing the application
+      overlay = nixpkgs.lib.composeManyExtensions [
+        poetry2nix.overlay
+        (final: prev: {
+          # The application
+          network_inventory = prev.poetry2nix.mkPoetryApplication {
+            projectDir = ./.;
+          };
+        })
+      ];
+    } // (flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        };
       in
       {
+        apps = {
+          network_inventory = pkgs.network_inventory;
+        };
+
+        defaultApp = pkgs.network_inventory;
+
         devShell = pkgs.mkShell {
           buildInputs = [
             pkgs.gnumake
-            pkgs.python39
             pkgs.python39Packages.poetry
+            pkgs.network_inventory
           ];
         };
-        shellHook = ''
-          export DJANGO_SETTINGS_MODULE=network_inventory.settings.local
-        '';
-      });
+      }));
 }
