3fd606a5ab547af7b8dc6f7df978eeddcbf74d23
Andreas Zweili <andreas@zweili.ch>
2022-11-19 18:10:59 +0100

    Rewrite to unstable

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A Python API for various tools I use at work.";
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
     flake-utils.url = github:numtide/flake-utils;
   };
   outputs = { self, nixpkgs, flake-utils }:
@@ -17,20 +17,35 @@
             pkgs.gnumake
             (pkgs.poetry2nix.mkPoetryEnv {
               projectDir = ./.;
-              overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend (self: super: {
-                findpython = super.findpython.overridePythonAttrs (
-                  old: {
-                    buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.pdm ];
-                  }
-                );
-                idna = super.idna.overridePythonAttrs (
-                  old: {
-                    buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.python39Packages.flit-core ];
-                  }
-                );
-              });
+              overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend
+                (self: super: {
+                  findpython = super.findpython.overridePythonAttrs (
+                    old: {
+                      buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.pdm ];
+                    }
+                  );
+                  django-floppyforms =
+                    super.django-floppyforms.overridePythonAttrs
+                      (
+                        old: {
+                          buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.python310Packages.setuptools ];
+                        }
+                      );
+                  django-crispy-forms = super.django-crispy-forms.overridePythonAttrs
+                    (
+                      old: {
+                        buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.python310Packages.setuptools ];
+                      }
+                    );
+                  exceptiongroup = super.exceptiongroup.overridePythonAttrs
+                    (
+                      old: {
+                        buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.python310Packages.flit-scm ];
+                      }
+                    );
+                });
             })
-            pkgs.python39Packages.poetry
+            pkgs.python310Packages.poetry
           ];
         };
         shellHook = ''
