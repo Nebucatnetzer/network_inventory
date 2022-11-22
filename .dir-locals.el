c934870b90b7f8c7746609b1696ceb2803a31bd2
Andreas Zweili <andreas@zweili.ch>
2022-11-22 14:14:36 +0100

    Update emacs dir variables

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((python-mode
-  (pyvenv-activate . "~/git_repos/projects/network_inventory/venv/")
-  (eval progn
-        (setenv "DJANGO_SETTINGS_MODULE" "network_inventory.settings.local")
-        (setenv "PYTEST_ADDOPTS" "-n 4 --nomigrations"))))
+((eval progn
+       (setenv "DJANGO_SETTINGS_MODULE" "network_inventory.settings.local")
+       (setenv "PYTEST_ADDOPTS" "-n 4 --nomigrations"))))
