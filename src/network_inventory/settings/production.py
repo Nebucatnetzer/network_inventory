0a669d7e88180a8487dacb512f39b23dae8bf315
Andreas Zweili <andreas@zweili.ch>
2022-12-12 10:07:29 +0100

    Correct name for SECRET_KEY env

--- src/network_inventory/settings/production.py
+++ src/network_inventory/settings/production.py
@@ -15,7 +15,7 @@ ALLOWED_HOSTS = [
 ]
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
