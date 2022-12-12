7466c392bc305d19c57a26b5801ffc7101ebe7e5
Andreas Zweili <andreas@zweili.ch>
2022-12-12 09:11:13 +0100

    First working Flake container

--- flake.nix
+++ flake.nix
@@ -28,6 +28,44 @@
           inherit system;
           overlays = [ self.overlays.default ];
         };
+        src = with pkgs.lib;
+          cleanSource (cleanSourceWith {
+            filter = name: type:
+              let
+                baseName = baseNameOf (toString name);
+              in
+                !(builtins.elem baseName [
+                  ".coverage"
+                  ".coveragerc"
+                  ".dir-locals.el"
+                  ".direnv"
+                  ".git"
+                  ".github"
+                  ".env"
+                  ".envrc"
+                  ".flake8"
+                  ".gitignore"
+                  ".gitlab-ci.yml"
+                  "docker-compose.yaml"
+                  "flake.lock"
+                  "flake.nix"
+                  "Makefile"
+                  "poetry.lock"
+                  "poetry.toml"
+                  "pyproject.toml"
+                  "pytest.ini"
+                ]);
+            src = ./.;
+          });
+        inventory = pkgs.stdenv.mkDerivation {
+          inherit src;
+          version = "latest";
+          pname = "network-inventory";
+          installPhase = ''
+            mkdir -p $out
+            cp -r ./src $out/code
+          '';
+        };
       in
       {
         devShells.default = pkgs.mkShell {
@@ -75,9 +113,63 @@
             '';
           };
         };
-        packages.venv = pkgs.inventoryEnv;
-        packages.inventory = pkgs.inventoryPackage;
-        packages.default = pkgs.inventoryPackage;
+        packages = {
+          venv = pkgs.inventoryEnv;
+          container = pkgs.dockerTools.buildImage {
+            name = "network-inventory";
+            tag = "latest";
+            created = "now";
+            copyToRoot = pkgs.buildEnv {
+              name = "image-root";
+              paths = [
+                pkgs.bashInteractive
+                pkgs.coreutils
+                pkgs.inventoryEnv
+                inventory
+                (pkgs.writeShellScriptBin "start-inventory" ''
+                  cd /code
+                  if [ -f .second_run ]; then
+                      sleep 2
+                      ${pkgs.python3}/bin/python manage.py collectstatic --noinput
+                      ${pkgs.python3}/bin/python manage.py makemigrations
+                      ${pkgs.python3}/bin/python manage.py migrate
+                  else
+                      ${pkgs.python3}/bin/python manage.py collectstatic --noinput
+                      ${pkgs.python3}/bin/python manage.py makemigrations backups
+                      ${pkgs.python3}/bin/python manage.py makemigrations computers
+                      ${pkgs.python3}/bin/python manage.py makemigrations core
+                      ${pkgs.python3}/bin/python manage.py makemigrations customers
+                      ${pkgs.python3}/bin/python manage.py makemigrations devices
+                      ${pkgs.python3}/bin/python manage.py makemigrations licenses
+                      ${pkgs.python3}/bin/python manage.py makemigrations nets
+                      ${pkgs.python3}/bin/python manage.py makemigrations softwares
+                      ${pkgs.python3}/bin/python manage.py makemigrations users
+                      ${pkgs.python3}/bin/python manage.py makemigrations
+                      ${pkgs.python3}/bin/python manage.py migrate
+                      ${pkgs.python3}/bin/python manage.py loaddata backups
+                      ${pkgs.python3}/bin/python manage.py loaddata computers
+                      ${pkgs.python3}/bin/python manage.py loaddata core
+                      ${pkgs.python3}/bin/python manage.py loaddata devices
+                      ${pkgs.python3}/bin/python manage.py loaddata nets
+                      ${pkgs.python3}/bin/python manage.py loaddata softwares
+                      ${pkgs.python3}/bin/python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')"
+                      touch .second_run
+                  fi
+                  ${pkgs.python310Packages.gunicorn}/bin/gunicorn network_inventory.wsgi:application --reload --bind 0.0.0.0:8000 --workers 3
+                '')
+              ];
+            };
+            config = {
+              Cmd = [ "start-inventory" ];
+              Env = [
+                "POSTGRES_DB=network_inventory"
+                "DJANGO_SETTINGS_MODULE=network_inventory.settings.production"
+                "PYTHONPATH=/lib/python3.10:/lib/python3.10/site-packages"
+              ];
+            };
+          };
+          default = self.packages.container;
+        };
       }));
 }
 
