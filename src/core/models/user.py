7bd564d29d8c126a3531ae1184b3fb116461944b
Andreas Zweili <andreas@zweili.ch>
2023-03-11 16:50:33 +0100

    Return the fullname for InventoryUser

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -2,4 +2,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class InventoryUser(AbstractUser):
-    pass
+    def __str__(self):
+        if self.first_name and self.last_name:
+            return self.first_name + " " + self.last_name
+        return self.username
