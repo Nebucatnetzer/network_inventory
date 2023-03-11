42dc703d836df3aa6f849fb8588129ba98e2cb4f
Andreas Zweili <andreas@zweili.ch>
2023-03-11 16:00:14 +0100

    Allow MailAlias to link to a group

--- src/users/models/mailalias.py
+++ src/users/models/mailalias.py
@@ -1,10 +1,14 @@
 from django.db import models
+from users.models import Group
 from users.models import Login
 
 
 class MailAlias(models.Model):
     email_address = models.EmailField()
     login = models.ForeignKey(Login, on_delete=models.CASCADE)
+    group = models.ForeignKey(
+        Group, null=True, blank=True, on_delete=models.SET_NULL
+    )
 
     def __str__(self):
         return self.email_address
