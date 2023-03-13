2e4795a37fa99f72183cb6b71b7a9dd77d13faf0
Andreas Zweili <andreas@zweili.ch>
2023-03-13 19:13:38 +0100

    Display Service in admin

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import InventoryUser
+from .models import Service
 
 admin.site.register(InventoryUser, UserAdmin)
+admin.site.register(Service)
