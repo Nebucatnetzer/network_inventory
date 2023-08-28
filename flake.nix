99bf6a1f589fbc8b6353b7663d9222e573da5b44
Andreas Zweili <andreas@zweili.ch>
2023-08-28 17:32:34 +0200

    Add env variable for LD_LIBRARY_PATH

--- flake.nix
+++ flake.nix
@@ -29,6 +29,7 @@
             (pkgs.writeScriptBin "dev" "${builtins.readFile ./dev.sh}")
           ];
           PYTHON_KEYRING_BACKEND = "keyring.backends.fail.Keyring";
+          LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
           shellHook = ''
             export DJANGO_SETTINGS_MODULE=network_inventory.settings.local
           '';
