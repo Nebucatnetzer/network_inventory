a79a52b42a756d298729f469417852f8b0e9e957
Andreas Zweili <andreas@zweili.ch>
2023-09-27 20:20:45 +0200

    Allow all hosts when developing
    The hostname changes a lot and some servers have multiple IPs.

--- src/network_inventory/settings/local.py
+++ src/network_inventory/settings/local.py
@@ -7,14 +7,7 @@ import os
 from .base import *
 
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "0.0.0.0",
-    getfqdn(),
-    gethostname(),
-    gethostbyname(gethostname()),
-]
+ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
