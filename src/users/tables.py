cfb0a09db1351781d0930ebdb00c8a81ddf95472
Andreas Zweili <andreas@zweili.ch>
2024-03-12 20:28:09 +0100

    Add login column

--- src/users/tables.py
+++ src/users/tables.py
@@ -14,7 +14,12 @@ class UsersTable(CoreTable):
         attrs={"td": {"class": "text-truncate", "style": "max-width: 150px;"}}
     )
     customer = tables.Column("Customer", linkify=True)
-    groups = tables.ManyToManyColumn()
+    logins = tables.ManyToManyColumn(
+        accessor=tables.A("user__login"), verbose_name="Logins"
+    )
+    groups = tables.ManyToManyColumn(
+        accessor=tables.A("group__login__user"), verbose_name="Groups"
+    )
     delete = tables.LinkColumn(
         "user_delete",
         text="delete",
